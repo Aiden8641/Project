60298c094038c959c97d2838039d4389d212d43e
Aiden8641 <90789887+Aiden8641@users.noreply.github.com>
2022-09-15 12:30:26 -0400

    stuff

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   input: document.getElementById("text"),
+  errmsg: document.querySelector(`.error`),
 };
 let Todo = [];
 
 function getinput() {
   DOMSelectors.button.addEventListener("click", function () {
     let display = DOMSelectors.input.value;
-    Todo.push(display);
-    console.log(display);
-    DOMSelectors.input.value = null;
+    if (display !== "") {
+      Todo.push(display);
+      console.log(display);
+      DOMSelectors.input.value = null;
+    }
+    if (display === "") {
+      console.log(DOMSelectors.errmsg);
+      DOMSelectors.errmsg.insertAdjacentHTML(
+        "beforeend",
+        `<p class="errmsg">Write A Task to add to the To Do list</p>`
+      );
+      console.log("Write A Task to add to the To Do List");
+    }
   });
 }
 
