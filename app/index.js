6662ff7b32bf2a060915284a63c28b4273cdb5dc
Aiden8641 <aiden18288@gmail.com>
2022-09-24 10:43:00 -0400

    work in porgress

--- app/index.js
+++ app/index.js
@@ -73,61 +73,111 @@ function add() {
   DOMSelectors.button.addEventListener("click", function () {
     let input = DOMSelectors.input.value;
     createList(input, Todo);
-    changeStatus();
   });
 }
 function changeStatus() {
   let complete = document.querySelectorAll(`.check`);
+
   complete.forEach((node) => {
+    console.log(node);
     node.addEventListener("click", function (e) {
-      if (e.status === "new") {
-        e.status = "In progress";
+      let dot = e.target.parentElement.parentElement.children[0].id;
+      let arrayitem = Todo.find((item) => item.text === dot);
+      console.log(arrayitem);
+      if (arrayitem.status === "new") {
+        arrayitem.status = "InProgress";
+        createList("change", Todo);
+        return arrayitem;
+      } else if (arrayitem.status === "InProgress") {
+        arrayitem.status = "Completed";
+        createList("change", Todo);
+        return arrayitem;
       }
-      console.log("status changed");
     });
   });
 }
-function remove() {}
+function remove() {
+  let remove = document.querySelectorAll(`.delete`);
+  remove.forEach((node) => {
+    node.addEventListener("click", function (e) {
+      let ridtis = Todo.find((item) => item.text === e.target.id);
+      e.target.parentElement.parentElement.remove();
+      console.log(ridtis);
+      Todo.splice(ridtis);
+
+      console.log("deleted");
+      console.log(Todo);
+    });
+  });
+}
 function createList(input, list) {
   let update = false;
-  if (DOMSelectors.input.value !== "") {
-    let obj = {
-      text: input,
-      status: "new",
-    };
-    list.push(obj);
+  if (input !== "") {
+    if (input !== "change") {
+      let obj = {
+        text: input,
+        status: "new",
+      };
+      list.push(obj);
+    }
+
     update = true;
     if (update === true) {
       while (DOMSelectors.new.firstChild) {
         DOMSelectors.new.removeChild(DOMSelectors.new.lastChild);
+        console.log("done1");
       }
       while (DOMSelectors.progress.firstChild) {
         DOMSelectors.progress.removeChild(DOMSelectors.progress.lastChild);
+        console.log("done2");
       }
       while (DOMSelectors.complete.firstChild) {
         DOMSelectors.complete.removeChild(DOMSelectors.complete.lastChild);
+        console.log("done3");
       }
+
       if (DOMSelectors.complete.firstChild === null) {
         list.forEach(function (e) {
           if (e.status === "new") {
             DOMSelectors.new.insertAdjacentHTML(
               "beforeend",
               `<div class="docont">
-                  <p class="dothis">${e.text}</p>
+                  <p  id="${e.text}"class="dothis">${e.text}</p>
                   <div class="btnCont">
                       <button id="${(e.text, e.status)}" class="check"></button>
                       <button id="${e.text}" class="delete"></button>
                   </div>        
                 </div>`
             );
-          } else if (e.status === "In Progress") {
+          } else if (e.status === "InProgress") {
+            DOMSelectors.progress.insertAdjacentHTML(
+              "beforeend",
+              `<div class="docont">
+                  <p id="${e.text}"class="dothis">${e.text}</p>
+                  <div class="btnCont">
+                      <button id="${(e.text, e.status)}" class="check"></button>
+                      <button id="${e.text}" class="delete"></button>
+                  </div>        
+                </div>`
+            );
             console.log(e);
           } else if (e.status === "Completed") {
+            DOMSelectors.complete.insertAdjacentHTML(
+              "beforeend",
+              `<div class="docont">
+                  <p id="${e.text}"class="dothis">${e.text}</p>
+                  <div class="btnCont">
+                      <button id="${e.text}" class="delete"></button>
+                  </div>        
+                </div>`
+            );
             console.log(e);
           }
         });
         update = false;
         DOMSelectors.input.value = null;
+        changeStatus();
+        remove();
       }
     }
   } else {
