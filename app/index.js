8533dde96f10b99c8f748006703792bda7f80536
Aiden8641 <aiden18288@gmail.com>
2022-09-24 15:35:31 -0400

    clean up

--- app/index.js
+++ app/index.js
@@ -6,73 +6,25 @@ const DOMSelectors = {
   progress: document.querySelector(`.inprogress`),
   complete: document.querySelector(`.completed`),
 };
-let Todo = [];
 
-/* function Write() {
-  Todo.forEach((e) => {
-    DOMSelectors.list.insertAdjacentHTML(
-      "beforeend",
-      `<div class="docont">
-        <p class="dothis">${e}</p>
-        <div class="btnCont">
-          <button id="${e}" class="check"></button>
-          <button id="${e}" class="delete"></button>
-        </div>        
-      </div>`
-    );
-    console.log(e);
-  });
-}
-function getrid() {
-  let remove = document.querySelectorAll(`.delete`);
-  remove.forEach((node) => {
-    node.addEventListener("click", function (e) {
-      e.target.parentElement.parentElement.remove();
-      Todo.splice(e.id);
-      console.log("deleted");
-    });
-  });
-}
-function check() {
-  let complete = document.querySelectorAll(`.check`);
-  complete.forEach((node) => {
-    node.addEventListener("click", function (e) {
-      e.target.parentElement.parentElement.children[0].className =
-        "dothiscomplete";
-      setTimeout(() => e.target.parentElement.parentElement.remove(), 1000);
-      Todo.splice(e.id);
-      console.log("completed");
-    });
-  });
-}
-
-function getinput() {
-  DOMSelectors.button.addEventListener("click", function () {
-    let display = DOMSelectors.input.value;
-    if (display !== "") {
-      DOMSelectors.errmsg.className = "errmsghide";
-      Todo.push(display);
-      DOMSelectors.input.value = null;
-      while (DOMSelectors.list.firstChild) {
-        DOMSelectors.list.removeChild(DOMSelectors.list.lastChild);
-      }
-      console.log(Todo);
-      Write();
-      getrid();
-      check();
-    }
-    if (display === "") {
-      DOMSelectors.errmsg.className = "errmsg";
-    }
-  });
-}
-
-getinput();
- */
 function add() {
   DOMSelectors.button.addEventListener("click", function () {
     let input = DOMSelectors.input.value;
     createList(input, Todo);
+  });
+}
+function remove() {
+  let remove = document.querySelectorAll(`.delete`);
+  remove.forEach((node) => {
+    node.addEventListener("click", function (e) {
+      let ridtis = Todo.find((item) => item.text === e.target.id);
+      e.target.parentElement.parentElement.remove();
+      console.log(ridtis);
+      Todo.splice(ridtis);
+
+      console.log("deleted");
+      console.log(Todo);
+    });
   });
 }
 function changeStatus() {
@@ -96,20 +48,8 @@ function changeStatus() {
     });
   });
 }
-function remove() {
-  let remove = document.querySelectorAll(`.delete`);
-  remove.forEach((node) => {
-    node.addEventListener("click", function (e) {
-      let ridtis = Todo.find((item) => item.text === e.target.id);
-      e.target.parentElement.parentElement.remove();
-      console.log(ridtis);
-      Todo.splice(ridtis);
-
-      console.log("deleted");
-      console.log(Todo);
-    });
-  });
-}
+const one = 1;
+let Todo = [];
 function createList(input, list) {
   let update = false;
   if (input !== "") {
